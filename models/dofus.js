4aa38628b186e7acd1c295baac07f9fbe5c40f7d
scoozy23 <scoozy23@gmail.com>
2022-11-24 15:46:36 +0100

    modele donjon

--- models/dofus.js
+++ models/dofus.js
@@ -1,8 +1,16 @@
 const mongoose = require('mongoose');
 
 const objectSchema = mongoose.Schema({
-    creationDate: {type: Date, required: false},
-    modificationDate: {type: Date, required: false},
+    name: {type: String, required: true},
+    level: {type: Number, required: true},
+    property: {type: String, required: false},
+    donjon: {type: Array, required: false},
+    difficulty: {type: Number, required: true},
+    success: {type: [String], required: true},
+    obtained: {type: Boolean, required: true},
+    stat: {type: String, required: true},
+    createDate: {type: Date, required: false},
+    updateDate: {type: Date, required: false},
     active: {type: Boolean, required: false}
 });
 
