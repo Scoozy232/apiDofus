719ced75f3f75b50768c73120abd9f86c932a1fe
florian <florian.aubin@laposte.net>
2022-11-24 15:41:26 +0100

    user + donjon fini

--- models/donjon.js
+++ models/donjon.js
@@ -1,4 +1,5 @@
 const mongoose = require('mongoose');
+const Quest = require('quest')
 
 const objectSchema = mongoose.Schema({
     creationDate: {type: Date, required: false},
@@ -9,7 +10,7 @@ const objectSchema = mongoose.Schema({
     success: {type: [String], required:true},
     technicalDifficulty: {type: String, required:true},
     linkedQuests: {type: [Quest], required:true},
-    done: {type: String, required:true}
+    done: {type: Boolean, required:true}
 });
 
 module.exports = mongoose.model('Donjon', objectSchema);
--- models/user.js
+++ models/user.js
@@ -1,4 +1,5 @@
 const mongoose = require('mongoose');
+const Donjon = require('donjon')
 
 const userSchema = mongoose.Schema({
     email: {type: String, required: true},
